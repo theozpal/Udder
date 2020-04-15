8121fe9da652d709a99d1c733a2709ee024b43ec
Ozan Pala <ozan.pala@windowslive.com>
2020-04-15 13:38:28 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ozan Pala on 2020-04-15.
+//  Copyright © 2020 Ozan Pala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
